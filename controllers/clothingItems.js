91fab768f18a59f93f78c5818937c4d82a089134
ajuarezse <ajuarez.se@gmail.com>
2024-10-09 13:24:04 -0700

    remove update item logic

--- controllers/clothingItems.js
+++ controllers/clothingItems.js
@@ -35,25 +35,6 @@ const getItems = (req, res) => {
     });
 };
 
-const updateItem = (req, res) => {
-  const { itemId } = req.params;
-  const { imageUrl } = req.body;
-
-  ClothingItem.findByIdAndUpdate(itemId, { $set: { imageUrl } })
-    .orFail()
-    .then((item) => res.status(200).send({ data: item }))
-    .catch((err) => {
-      console.error(err);
-      if (err.name === "CastError") {
-        return res.status(BAD_REQUEST_STATUS).send({ message: err.message });
-      }
-      if (err.name === "DocumentNotFoundError") {
-        return res.status(NOT_FOUND_STATUS).send({ message: err.message });
-      }
-      return res.status(500).send({ message: "Error from updateItem", err });
-    });
-};
-
 const deleteItem = (req, res) => {
   const { itemId } = req.params;
 
@@ -120,7 +101,6 @@ const disLikeItem = (req, res) => {
 module.exports = {
   createItem,
   getItems,
-  updateItem,
   deleteItem,
   likeItem,
   disLikeItem,
--- routes/clothingItems.js
+++ routes/clothingItems.js
@@ -2,7 +2,6 @@ const router = require("express").Router();
 const {
   createItem,
   getItems,
-  updateItem,
   deleteItem,
   likeItem,
   disLikeItem,
@@ -11,7 +10,6 @@ const {
 router.get("/", getItems);
 router.post("/", createItem);
 router.delete("/:itemId", deleteItem);
-router.put("/:itemId", updateItem);
 router.put("/:itemId/likes", likeItem);
 router.delete("/:itemId/likes", disLikeItem);
 
