16a2f481a172799c35a651522a44a3b87825a61c
ajuarezse <ajuarez.se@gmail.com>
2024-10-21 13:43:22 -0700

    add token verification

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,3 +1,18 @@
-const token = authorization.replace("Bearer ", "");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = require("../utils/config");
 
-payload = jwt.verify(token, JWT_SECRET);
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith("Bearer")) {
+    return res.status(401).send({ message: "Authorization required" });
+  }
+  const token = authorization.replace("Bearer ", "");
+  let payload;
+
+  try {
+    payload = jwt.verify(token, JWT_SECRET);
+  } catch (err) {
+    return res.status(401).send({ message: "Authorization required" });
+  }
+};
