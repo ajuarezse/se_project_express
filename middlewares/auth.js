71ce972e03e834948a67556cb6bbc432f341a6ce
ajuarezse <ajuarez.se@gmail.com>
2024-10-21 14:39:31 -0700

    route auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,11 +1,14 @@
 const jwt = require("jsonwebtoken");
-const JWT_SECRET = require("../utils/config");
+const { JWT_SECRET } = require("../utils/config");
+const { AUTHENTICATION_ERROR } = require("../utils/errors");
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith("Bearer")) {
-    return res.status(401).send({ message: "Authorization required" });
+    return res
+      .status(AUTHENTICATION_ERROR)
+      .send({ message: "Authorization required" });
   }
   const token = authorization.replace("Bearer ", "");
   let payload;
@@ -13,6 +16,10 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
-    return res.status(401).send({ message: "Authorization required" });
+    return res
+      .status(AUTHENTICATION_ERROR)
+      .send({ message: "Authorization required" });
   }
+  req.user = payload;
+  next();
 };
