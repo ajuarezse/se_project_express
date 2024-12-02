3e23efc746fca5f8f6854532ef919035ba7074f5
ajuarezse <ajuarez.se@gmail.com>
2024-12-02 13:03:54 -0800

    refactor handler

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const mainRouter = require("./routes/index");
-const errorHandler = require("./middlewares/errorHandler");
+const errorHandler = require("./middlewares/error-handler");
 
 const app = express();
 const { PORT = 3001 } = process.env;
