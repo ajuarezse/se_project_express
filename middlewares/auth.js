69f2ba29fe830779bfbdd5ebc4019109866d6cf1
ajuarezse <ajuarez.se@gmail.com>
2024-12-09 16:28:45 -0800

    add unauth logic

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,25 +1,23 @@
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../utils/config");
-const { AUTHENTICATION_ERROR } = require("../utils/errors");
+const UnauthorizedError = require("../utils/UnauthorizedError");
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith("Bearer")) {
-    return res
-      .status(AUTHENTICATION_ERROR)
-      .send({ message: "Authorization required" });
+    return next(new UnauthorizedError("Authorization required"));
   }
+
   const token = authorization.replace("Bearer ", "");
   let payload;
 
   try {
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
-    return res
-      .status(AUTHENTICATION_ERROR)
-      .send({ message: "Authorization required" });
+    return next(new UnauthorizedError("Authorization required"));
   }
+
   req.user = payload;
   return next();
 };
